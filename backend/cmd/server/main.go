dddbf73df3fa36fc23a423374c08046d50345627
Folkchayutpong <folkchayutpong@gmail.com>
2025-06-16 18:04:34 +0700

    initial server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/nack098/nakumanager/internal/auth"
+	"github.com/nack098/nakumanager/internal/routes"
+	"github.com/nack098/nakumanager/internal/ws"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	api := app.Group("/api")
+
+	// Auth
+	api.Post("/login", auth.Login)
+	api.Post("/register", auth.Register)
+
+	//workspace
+	api.Post("/workspaces", routes.CreateWorkspace)
+	api.Get("/workspaces", routes.GetWorkspacesByID)
+	api.Delete("/workspaces", routes.DeleteWorkspace)
+
+	// Project
+	api.Post("/projects", routes.CreateProject)
+	api.Get("/projects", routes.GetProjectsByID)
+	api.Delete("/projects", routes.DeleteProject)
+
+	// Team
+	api.Post("/teams", routes.CreateTeam)
+	api.Get("/teams", routes.GetTeamsByID)
+	api.Delete("/teams", routes.DeleteTeam)
+
+	// Task
+	api.Post("/issues", routes.CreateIssue)
+	api.Get("/issues", routes.GetIssuesByID)
+	api.Delete("/issues", routes.DeleteIssue)
+
+	// View
+	api.Post("/views", routes.CreateView)
+	api.Get("/views", routes.GetViewsByID)
+	api.Delete("/views", routes.DeleteView)
+
+	// Websocket
+	wsGroup := app.Group("/ws", ws.WebSocketMiddleware)
+	wsGroup.Get("/workspaces", websocket.New(ws.UpdateWorkspaceHandler))
+	wsGroup.Get("/projects", websocket.New(ws.UpdateProjectHandler))
+	wsGroup.Get("/issues", websocket.New(ws.UpdateIssueHandler))
+	wsGroup.Get("/views", websocket.New(ws.UpdateViewHandler))
+	wsGroup.Get("/teams", websocket.New(ws.UpdateTeamHandler))
+
+	app.Listen(":3000")
 }
