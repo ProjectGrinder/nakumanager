03a7eb2f6a5e659bc58295e2d2572911a4ce4b59
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-06-18 23:55:31 +0700

    feat(login & register): setup login and register

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Login() {
+  //Implement the login functionality here
+  return <h1>Login</h1>;
+}
---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function Register() {
+  //Implement the register functionality here
+  return <h1>Register</h1>;
+}
