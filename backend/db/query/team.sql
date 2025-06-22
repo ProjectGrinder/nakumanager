e330252b3188551de4342075c4cb21c6b93b03b4
Folkchayutpong <folkchayutpong@gmail.com>
2025-06-22 21:34:23 +0700

    feat(team,workspace): add query function

--- backend/db/query/team.sql
+++ backend/db/query/team.sql
@@ -1,17 +1,26 @@
 -- name: CreateTeam :exec
-INSERT INTO teams (id, name, leader_id)
+INSERT INTO teams (id, name, workspace_id)
 VALUES (?, ?, ?);
 
 -- name: GetTeamByID :one
-SELECT id, name, leader_id
+SELECT id, name, workspace_id, leader_id
 FROM teams
 WHERE id = ?;
+
+-- name: GetTeamsByUserID :many
+SELECT t.id, t.name, t.workspace_id, t.leader_id
+FROM teams t
+JOIN team_members tm ON t.id = tm.team_id
+WHERE tm.user_id = ?;
 
 -- name: DeleteTeam :exec
 DELETE FROM teams WHERE id = ?;
 
+-- name: DeleteTeamFromTeamMembers :exec
+DELETE FROM team_members WHERE team_id = ?;
+
 -- name: ListTeams :many
-SELECT id, name, leader_id
+SELECT id, name, workspace_id, leader_id
 FROM teams
 ORDER BY name;
 
@@ -24,7 +33,29 @@ DELETE FROM team_members
 WHERE team_id = ? AND user_id = ?;
 
 -- name: ListTeamMembers :many
-SELECT u.*
+SELECT u.id, u.username, u.email
 FROM users u
 JOIN team_members tm ON u.id = tm.user_id
 WHERE tm.team_id = ?;
+
+-- name: UpdateTeam :exec
+UPDATE teams
+SET name = ?, leader_id = ?
+WHERE id = ?;
+
+-- name: GetOwnerByTeamID :one
+SELECT w.owner_id
+FROM teams t
+JOIN workspaces w ON t.workspace_id = w.id
+WHERE t.id = ?;
+
+
+-- name: GetLeaderByTeamID :one
+SELECT leader_id
+FROM teams
+WHERE id = ?;
+
+-- name: IsMemberInTeam :one
+SELECT COUNT(*) AS count
+FROM team_members
+WHERE team_id = ? AND user_id = ?;
