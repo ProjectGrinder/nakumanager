98b84a350b2a06d0e6c0c394b7388b8460148b4b
Folkchayutpong <folkchayutpong@gmail.com>
2025-06-23 14:42:24 +0700

    feat(issue): add AddAssigneeToIssue and REmoveAssigneeFromIssue routh path

--- backend/internal/gateway/issue.go
+++ backend/internal/gateway/issue.go
@@ -7,6 +7,9 @@ import (
 
 func SetUpIssueRoutes(api fiber.Router, h *routes.IssueHandler) {
 	api.Post("/issues", h.CreateIssue)
+	api.Post("/issues/assignee", h.AddAssigneeToIssue)
+	api.Delete("/issues/assignee", h.RemoveAssigneeFromIssue)
 	api.Get("/issues/:id", h.GetIssuesByUserID)
 	api.Delete("/issues/:id", h.DeleteIssue)
+
 }
