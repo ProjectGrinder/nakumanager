bc3d467c7f6c6ada6c575edab675608011490005
Folkchayutpong <folkchayutpong@gmail.com>
2025-06-23 14:40:33 +0700

    feat(issue): add another fields in issue

--- backend/db/migrations/000006_create_issue_table.up.sql
+++ backend/db/migrations/000006_create_issue_table.up.sql
@@ -3,22 +3,24 @@ CREATE TABLE issues (
     title TEXT NOT NULL,
     content TEXT,
     priority TEXT CHECK(priority IN ('low', 'medium', 'high')),
-    status TEXT CHECK(status IN ('todo', 'doing', 'done')),
-    project_id INTEGER,
-    team_id INTEGER NOT NULL,
+    status TEXT CHECK(status IN ('todo', 'doing', 'done')) NOT NULL,
+    assignee TEXT,
+    project_id TEXT,
+    team_id TEXT NOT NULL,
     start_date DATETIME,
     end_date DATETIME,
     label TEXT,
     owner_id TEXT NOT NULL,
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (team_id) REFERENCES teams(id),
+    FOREIGN KEY (assignee) REFERENCES users(id),
     FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE issue_assignees (
-    issue_id TEXT,
-    user_id TEXT,
+    issue_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
     PRIMARY KEY (issue_id, user_id),
-    FOREIGN KEY (issue_id) REFERENCES issues(id),
+    FOREIGN KEY (issue_id) REFERENCES issues(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
