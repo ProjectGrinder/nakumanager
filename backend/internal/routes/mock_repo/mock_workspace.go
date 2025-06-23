0bd0f45a882676918f7ca31b20d78f8f502fd253
Folkchayutpong <folkchayutpong@gmail.com>
2025-06-23 14:44:41 +0700

    feat: add mock_repo for test

---
+++ backend/internal/routes/mock_repo/mock_workspace.go
@@ -0,0 +1,63 @@
+package mock
+
+import (
+	"context"
+
+	"github.com/nack098/nakumanager/internal/db"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockWorkspaceRepo struct {
+	mock.Mock
+}
+
+func (m *MockWorkspaceRepo) CreateWorkspace(ctx context.Context, id string, name string, ownerID string) error {
+	args := m.Called(ctx, id, name, ownerID)
+	return args.Error(0)
+}
+
+func (m *MockWorkspaceRepo) GetWorkspaceByID(ctx context.Context, id string) (db.Workspace, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(db.Workspace), args.Error(1)
+}
+
+func (m *MockWorkspaceRepo) DeleteWorkspace(ctx context.Context, id string) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (m *MockWorkspaceRepo) ListWorkspaceMembers(ctx context.Context, workspaceID string) ([]db.User, error) {
+	args := m.Called(ctx, workspaceID)
+	return args.Get(0).([]db.User), args.Error(1)
+}
+
+func (m *MockWorkspaceRepo) AddMemberToWorkspace(ctx context.Context, workspaceID, userID string) error {
+	args := m.Called(ctx, workspaceID, userID)
+	return args.Error(0)
+}
+
+func (m *MockWorkspaceRepo) RemoveMemberFromWorkspace(ctx context.Context, workspaceID, userID string) error {
+	args := m.Called(ctx, workspaceID, userID)
+	return args.Error(0)
+}
+
+func (m *MockWorkspaceRepo) RenameWorkspace(ctx context.Context, id string, newName string) error {
+	args := m.Called(ctx, id, newName)
+	return args.Error(0)
+}
+
+func (m *MockWorkspaceRepo) ListWorkspacesWithMembersByUserID(ctx context.Context, userID string) ([]db.ListWorkspacesWithMembersByUserIDRow, error) {
+	args := m.Called(ctx, userID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]db.ListWorkspacesWithMembersByUserIDRow), args.Error(1)
+}
+
+func (m *MockWorkspaceRepo) GetWorkspaceByUserID(ctx context.Context, userID string) ([]db.Workspace, error) {
+	args := m.Called(ctx, userID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]db.Workspace), args.Error(1)
+}
