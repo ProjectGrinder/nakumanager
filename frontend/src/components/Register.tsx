995293e1db1eb5cbdaf18140ec1d66518b22e509
Pooh5821 <pooh5821ingkanunt@gmail.com>
2025-07-07 17:02:56 +0700

    fix: edit login and register page

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -41,56 +41,59 @@ export default function Register() {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-between h-140 w-100 text-white font-bold p-10 bg-gray-700 rounded-2xl">
-        <h1 className="text-3xl pb-6">Register</h1>
-        <div className="flex flex-col w-full items-center">
-          <div className="w-full m-4">
-            <p className="text-sm pb-2">Username</p>
+      <div className="flex flex-col items-center justify-between h-160 w-120 text-white font-bold p-10 bg-gray-700 rounded-2xl">
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl text-gray-200 pb-4">Nakumanager</h1>
+          <p className="text-sm text-gray-400 pb-6">
+            The management software that grows with your need
+          </p>
+        </div>
+        <div className="flex flex-col w-full items-center mb-15">
+          <span className="text-2xl font-semibold text-gray-200">Register</span>
+          <div className="w-full m-2">
+            <p className="text-sm pb-2 text-gray-200">Username</p>
             <input
               id="username"
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full bg-gray-100 border-4 border-gray-400 text-gray-700 px-4 py-2 text-base rounded-lg outline-none"
-              placeholder="Enter username"
+              className="w-full bg-gray-200 border-2 border-gray-400 text-gray-700 px-4 py-2 text-sm rounded-xl outline-none"
             />
           </div>
 
-          <div className="w-full m-4">
-            <p className="text-sm pb-2">Email</p>
+          <div className="w-full m-2">
+            <p className="text-sm pb-2 text-gray-200">Email</p>
             <input
               id="email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full bg-gray-100 border-4 border-gray-400 text-gray-700 px-4 py-2 text-base rounded-lg outline-none"
-              placeholder="Enter email"
+              className="w-full bg-gray-200 border-2 border-gray-400 text-gray-700 px-4 py-2 text-sm rounded-xl outline-none"
             />
           </div>
 
-          <div className="w-full m-4">
-            <p className="text-sm pb-2">Password</p>
+          <div className="w-full m-2">
+            <p className="text-sm pb-2 text-gray-200">Password</p>
             <input
               id="password"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full bg-gray-100 border-4 border-gray-400 text-gray-700 px-4 py-2 text-base rounded-lg outline-none"
-              placeholder="Enter password"
+              className="w-full bg-gray-200 border-2 border-gray-400 text-gray-700 px-4 py-2 text-sm rounded-xl outline-none"
             />
           </div>
 
           <button
             id="btn-check"
             onClick={handleRegister}
-            className="mt-6 mb-10 w-40 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            className="mt-6 border-box px-10 py-4 bg-blue-500 text-lg text-white rounded-xl hover:bg-blue-700"
           >
-            Register
+            Confirm
           </button>
         </div>
         <p className="text-color-100 font-normal text-sm">
           Back to
-          <Link href="/login" className="ml-2 underline">
+          <Link href="/" className="ml-2 underline">
             Log in
           </Link>
         </p>
