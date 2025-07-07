9fb126c6b212414b02ca05aa54da856dd6dfebb8
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-07 18:00:27 +0700

    feat: add gateway

--- backend/internal/gateway/team.go
+++ backend/internal/gateway/team.go
@@ -8,6 +8,10 @@ import (
 func SetUpTeamRoutes(api fiber.Router, h *routes.TeamHandler) {
 	api.Post("/teams", h.CreateTeam)
 	api.Get("/teams", h.GetTeamsByUserID)
+	api.Post("/teams/:id/members", h.AddMemberToTeam)
+	api.Delete("/teams/:id/members", h.RemoveMemberFromTeam)
+	api.Post("/teams/:id/rename", h.RenameTeam)
+	api.Post("/teams/:id/leader", h.SetTeamLeader)
 	api.Delete("/teams/:id", h.DeleteTeam)
 
 }
