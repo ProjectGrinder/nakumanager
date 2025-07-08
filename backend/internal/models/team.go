013c60fa77cd8172a89d3fcb02ced5508855134e
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-08 16:38:16 +0700

    feat: add team models

--- backend/internal/models/team.go
+++ backend/internal/models/team.go
@@ -1,29 +1,14 @@
 package model
 
-import "database/sql"
-
 type CreateTeam struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	WorkspaceID string `json:"workspace_id"`
 }
 
-type AddMemberToTeam struct {
-	TeamID string `json:"team_id"`
-	UserID string `json:"user_id"`
-}
-
-type RemoveMemberFromTeam struct {
-	TeamID string `json:"team_id"`
-	UserID string `json:"user_id"`
-}
-
-type RenameTeam struct {
-	TeamID string `json:"team_id"`
-	Name   string `json:"name"`
-}
-
-type SetTeamLeader struct {
-	TeamID   string         `json:"id"`
-	LeaderID sql.NullString `json:"leader_id"`
+type UpdateTeamRequest struct {
+	Name        *string   `json:"Name"`
+	AddMembers    *[]string `json:"add_members"`
+	RemoveMembers *[]string `json:"remove_members"`
+	NewLeaderID   *string   `json:"new_leader_id"`
 }
