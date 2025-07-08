d954d0f8687f5fbf7e5f5831362f98a0d4d744e9
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-08 16:38:35 +0700

    feat: add repository function

--- backend/internal/repositories/team.go
+++ backend/internal/repositories/team.go
@@ -2,17 +2,14 @@ package repositories
 
 import (
 	"context"
-	"database/sql"
-	"errors"
-	"fmt"
 
 	"github.com/nack098/nakumanager/internal/db"
 	models "github.com/nack098/nakumanager/internal/models"
 )
 
 type TeamRepository interface {
-	AddMemberToTeam(ctx context.Context, data models.AddMemberToTeam) error
-	RemoveMemberFromTeam(ctx context.Context, data models.RemoveMemberFromTeam) error
+	AddMemberToTeam(ctx context.Context, data db.AddMemberToTeamParams) error
+	RemoveMemberFromTeam(ctx context.Context, data db.RemoveMemberFromTeamParams) error
 	CreateTeam(ctx context.Context, data models.CreateTeam) error
 	DeleteTeam(ctx context.Context, id string) error
 	GetTeamByID(ctx context.Context, id string) (db.Team, error)
@@ -23,8 +20,8 @@ type TeamRepository interface {
 	GetLeaderByTeamID(ctx context.Context, userID string) (string, error)
 	IsMemberInTeam(ctx context.Context, teamID, userID string) (bool, error)
 	IsTeamExists(ctx context.Context, teamID string) (bool, error)
-	RenameTeam(ctx context.Context, data models.RenameTeam) error
-	SetLeaderToTeam(ctx context.Context, data models.SetTeamLeader) error
+	RenameTeam(ctx context.Context, data db.RenameTeamParams) error
+	SetLeaderToTeam(ctx context.Context, data db.SetLeaderToTeamParams) error
 }
 
 type teamRepo struct {
@@ -35,20 +32,12 @@ func NewTeamRepository(q *db.Queries) TeamRepository {
 	return &teamRepo{queries: q}
 }
 
-func (r *teamRepo) AddMemberToTeam(ctx context.Context, data models.AddMemberToTeam) error {
-	model := db.AddMemberToTeamParams{
-		TeamID: data.TeamID,
-		UserID: data.UserID,
-	}
-	return r.queries.AddMemberToTeam(ctx, model)
+func (r *teamRepo) AddMemberToTeam(ctx context.Context, data db.AddMemberToTeamParams) error {
+	return r.queries.AddMemberToTeam(ctx, data)
 }
 
-func (r *teamRepo) RemoveMemberFromTeam(ctx context.Context, data models.RemoveMemberFromTeam) error {
-	model := db.RemoveMemberFromTeamParams{
-		TeamID: data.TeamID,
-		UserID: data.UserID,
-	}
-	return r.queries.RemoveMemberFromTeam(ctx, model)
+func (r *teamRepo) RemoveMemberFromTeam(ctx context.Context, data db.RemoveMemberFromTeamParams) error {
+	return r.queries.RemoveMemberFromTeam(ctx, data)
 }
 
 func (r *teamRepo) CreateTeam(ctx context.Context, data models.CreateTeam) error {
@@ -86,17 +75,9 @@ func (r *teamRepo) GetOwnerByTeamID(ctx context.Context, teamID string) (string,
 }
 
 func (r *teamRepo) GetLeaderByTeamID(ctx context.Context, teamID string) (string, error) {
-	leader, err := r.queries.GetLeaderByTeamID(ctx, teamID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return "", fmt.Errorf("team %s not found", teamID)
-		}
-		return "", err
-	}
-	if !leader.Valid {
-		return "", nil
-	}
-	return leader.String, nil
+	// leader, err := r.queries.GetLeaderByTeamID(ctx, teamID)
+	// return leader, err
+	return "", nil
 }
 
 func (r *teamRepo) IsMemberInTeam(ctx context.Context, teamID, userID string) (bool, error) {
@@ -118,14 +99,10 @@ func (r *teamRepo) IsTeamExists(ctx context.Context, teamID string) (bool, error
 	return count > 0, nil
 }
 
-func (r *teamRepo) RenameTeam(ctx context.Context, data models.RenameTeam) error {
-	model := db.RenameTeamParams{ID: data.TeamID, Name: data.Name}
-	return r.queries.RenameTeam(ctx, model)
+func (r *teamRepo) RenameTeam(ctx context.Context, data db.RenameTeamParams) error {
+	return r.queries.RenameTeam(ctx, data)
 }
 
-func (r *teamRepo) SetLeaderToTeam(ctx context.Context, data models.SetTeamLeader) error {
-	return r.queries.SetLeaderToTeam(ctx, db.SetLeaderToTeamParams{
-		LeaderID: data.LeaderID,
-		ID:       data.TeamID,
-	})
+func (r *teamRepo) SetLeaderToTeam(ctx context.Context, data db.SetLeaderToTeamParams) error {
+	return r.queries.SetLeaderToTeam(ctx, data)
 }
