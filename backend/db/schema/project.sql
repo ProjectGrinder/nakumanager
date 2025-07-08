dd3554cb9b863c2702247e36de249a91765e7ff6
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-08 15:37:07 +0700

    fix: fix schema

--- backend/db/schema/project.sql
+++ backend/db/schema/project.sql
@@ -1,20 +1,20 @@
 CREATE TABLE projects (
     id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
-    status TEXT,
-    priority TEXT,
+    status TEXT NULL,
+    priority TEXT NULL,
     workspace_id TEXT NOT NULL,
     team_id TEXT NOT NULL,
-    leader_id TEXT,
-    start_date DATETIME,
-    end_date DATETIME,
-    label TEXT,
+    leader_id TEXT NULL,
+    start_date DATETIME NULL,
+    end_date DATETIME NULL,
+    label TEXT NULL,
     created_by TEXT NOT NULL,
 
-    FOREIGN KEY (workspace_id) REFERENCES workspaces(id),
-    FOREIGN KEY (team_id) REFERENCES teams(id),
-    FOREIGN KEY (leader_id) REFERENCES users(id),
-    FOREIGN KEY (created_by) REFERENCES users(id)
+    FOREIGN KEY (workspace_id) REFERENCES workspaces(id) ON DELETE CASCADE,
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
+    FOREIGN KEY (leader_id) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE project_members (
