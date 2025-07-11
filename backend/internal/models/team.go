da16f5ea77fd324b6755db948d4f4d87fc950561
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-11 17:50:14 +0700

    feat: add validation of schema fields

--- backend/internal/models/team.go
+++ backend/internal/models/team.go
@@ -2,12 +2,12 @@ package model
 
 type CreateTeam struct {
 	ID          string `json:"id"`
-	Name        string `json:"name"`
-	WorkspaceID string `json:"workspace_id"`
+	Name        string `json:"name" validate:"required"`
+	WorkspaceID string `json:"workspace_id" validate:"required"`
 }
 
 type UpdateTeamRequest struct {
-	Name        *string   `json:"Name"`
+	Name          *string   `json:"Name"`
 	AddMembers    *[]string `json:"add_members"`
 	RemoveMembers *[]string `json:"remove_members"`
 	NewLeaderID   *string   `json:"new_leader_id"`
